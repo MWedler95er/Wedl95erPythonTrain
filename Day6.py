a9d834860fd712c4af8f4015807920f0076f78bc
MWedl95er <michael-wedler95@gmx.de>
2026-01-29 17:14:07 +0100

    list update

---
+++ Day6.py
@@ -0,0 +1,31 @@
+def greet_user(name):
+    print(f"Hello, {name}")
+
+def greet_user(name="Guest"):
+    print(f"Hello, {name}")
+
+greet_user()
+greet_user("Michael")
+
+
+def calculation_rectangle(length, breadth):
+    return print(length*breadth)
+
+calculation_rectangle(10,2)
+
+
+coder_name = "MiKZ"
+
+def name_the_coder(coder_name="Michael"):
+    print(coder_name)
+
+name_the_coder()
+print(coder_name)
+
+def rename_the_coder():
+    global coder_name
+    coder_name = "michi"
+    print(coder_name)
+
+rename_the_coder()
+print(coder_name)
