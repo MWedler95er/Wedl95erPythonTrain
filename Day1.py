a2bcb1392a6172709f159a80a6e26f780ff3e2a6
MWedl95er <michael-wedler95@gmx.de>
2026-01-30 16:38:04 +0100

    + DAY 13

--- Day1.py
+++ Day1.py
@@ -42,3 +42,29 @@ def odd_or_even_sec(number):
         return "Odd"
 print(odd_or_even_sec(3))
 
+#pythonic way
+def odd_or_even_sec(number):
+    return "even" if numer % 2 == 0 else "odd"
+
+#pythonic way 2
+def odd_or_even_sec(number):
+    return ("Even", "Odd")[number % 2]
+
+""" DAY 13 """
+def biggest_number_of_three(number1, number2, number3):
+    largest_nr = 0
+    if number1 > number2:
+        largest_nr = number1
+    else:
+        largest_nr = number2
+    if largest_nr < number3:
+        largest_nr = number2
+    print(largest_nr)
+biggest_number_of_three(2,15,5)
+
+#pythonic way
+def largest_of_3(number1, number2, number3):
+    largest_nr = max(number1, number2, number3)
+    return largest_nr
+print(largest_of_3(2,15,10))
+
