fe0e521e898c7135a4d1452fc6233a9a0177d772
MWedl95er <michael-wedler95@gmx.de>
2026-01-30 16:24:37 +0100

    + DAY 12

--- Day1.py
+++ Day1.py
@@ -25,3 +25,20 @@ while counter < 20:
     if counter%2==0:
         print(counter)
     counter+=1
+
+''' DAY 12 '''
+def odd_or_even(number):
+    if number % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+odd_or_even(3)
+
+# if method return some thin it need to Printet 
+def odd_or_even_sec(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+print(odd_or_even_sec(3))
+
