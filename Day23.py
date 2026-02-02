0467c0c435d65b68b7945e91b641d3974b6a58af
MWedl95er <michael-wedler95@gmx.de>
2026-02-02 10:39:13 +0100

    DAY 24

--- Day23.py
+++ Day23.py
@@ -7,13 +7,13 @@ list_objk2 = [ 10,2,3,4,5]
 def interselection_objk(list_objk1,list_objk2):
     objk1 = set(list_objk1)
     objk2 = set(list_objk2)
-    return objk1.intersection(objk2)
+    return objk1.intersection(objk2) #!! unsauber gibt set zurück kein list
 
 print(interselection_objk(list_objk1,list_objk2))
 
 def interselection_objk2(list_objk1,list_objk2):
     objk1, objk2 = set(list_objk1), set(list_objk2)
-    return objk1.intersection(objk2)
+    return list(objk1.intersection(objk2))
 
 print(interselection_objk2(list_objk1,list_objk2))
 
@@ -23,3 +23,13 @@ def interselection_objk_py_way(list_objk1,list_objk2):
     return list(set(list_objk1)& set(list_objk2))
 
 print(interselection_objk_py_way(list_objk1,list_objk2))
+
+''' DAY 24 '''
+
+list_w = ["Hallo", "ich", "mag", "python"]
+def sentence_maker(list_w):
+    sentence = ""
+    for x in list_w:
+        sentence += x+" "
+    return sentence
+print(sentence_maker(list_w))
