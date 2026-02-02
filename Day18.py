9adf949750429b8785fd8432526043a6cca0755a
MWedl95er <michael-wedler95@gmx.de>
2026-02-02 08:10:57 +0100

    +Day 19

--- Day18.py
+++ Day18.py
@@ -1,7 +1,17 @@
+""" DAY 18 """
+#Write a function to find the sum of all elements in a list.
+# pythonic way
 def list_sum(list_objekt):
     return sum(list_objekt)
 
 testlist=[1,2,3,4,5]
 print(list_sum(testlist))
-testlist=["hallo","hello","hola"]
-print(list_sum(testlist))
+
+
+''' DAY 19 '''
+#Write a function to find the maximum element in a list.
+
+def max_element(list_of_numbers):
+    return max(list_of_numbers)
+
+print(max_element(testlist))
