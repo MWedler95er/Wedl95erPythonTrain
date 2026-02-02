331dc88e863efefe4cf226f58140b0be53d18c0a
MWedl95er <michael-wedler95@gmx.de>
2026-02-02 11:13:05 +0100

    DAY 25 - not korrekt

--- Day23.py
+++ Day23.py
@@ -25,7 +25,7 @@ def interselection_objk_py_way(list_objk1,list_objk2):
 print(interselection_objk_py_way(list_objk1,list_objk2))
 
 ''' DAY 24 '''
-
+# write a funcion to convert a list of words into a sentence
 list_w = ["Hallo", "ich", "mag", "python"]
 def sentence_maker(list_w):
     sentence = ""
@@ -33,3 +33,23 @@ def sentence_maker(list_w):
         sentence += x+" "
     return sentence
 print(sentence_maker(list_w))
+
+#pytonic way
+def sentence_maker(list_w):
+    sentence = " ".join(list_w)   # join -> only for list with strings in it -> return a new String
+    return f"{sentence.capitalize()}."  # .capitalize => fist letter Big!! 
+
+""" DAY 25 """
+sentece = "That is a test String, an it is a bad String"
+def word_conter(sentence):
+    splitet = sentence.split()
+    count_frequence = {}
+    for x in splitet:
+        if count_frequence.keys() == x:
+            count_frequence[x]=count_frequence.get(x) +1 
+        else:
+            count_frequence[x]=1
+    return count_frequence
+            
+
+print(word_conter(sentece))
