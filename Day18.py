240ba8f3768fdc49be9475793ab8a1de692011b8
MWedl95er <michael-wedler95@gmx.de>
2026-02-02 08:56:35 +0100

    fibunacci sequence

--- Day18.py
+++ Day18.py
@@ -1,6 +1,5 @@
 """ DAY 18 """
 #Write a function to find the sum of all elements in a list.
-# pythonic way
 def list_sum(list_objekt):
     return sum(list_objekt)
 
@@ -15,3 +14,26 @@ def max_element(list_of_numbers):
     return max(list_of_numbers)
 
 print(max_element(testlist))
+
+def fibonacci_sequence(max_value):
+    number = [ 1, 1, 0]
+    loop_condition = 1
+    while loop_condition <= max_value: 
+        number[2] = number[0] + number[1]
+        number[0],number[1] = number[1],number[2]
+        if loop_condition + number[2] < max_value:
+            loop_condition = number[2]
+        else:
+            return number[2]
+
+print(fibonacci_sequence(174))
+
+# Genetator ansatz
+def fibonacci_generator(max_value):
+    a,b = 0,1
+    while a <= max_value:
+        yield a
+        a,b = b, a+b
+
+for x in fibonacci_generator(235):
+    print(x)
