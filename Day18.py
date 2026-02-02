2e225fdafe89b81604a6ba108164442ee7b10c3e
MWedl95er <michael-wedler95@gmx.de>
2026-02-02 09:40:14 +0100

    DAY 21

--- Day18.py
+++ Day18.py
@@ -3,7 +3,7 @@
 def list_sum(list_objekt):
     return sum(list_objekt)
 
-testlist=[1,2,3,4,5]
+testlist=[1,3,5,8,13]
 print(list_sum(testlist))
 
 
@@ -37,3 +37,48 @@ def fibonacci_generator(max_value):
 
 for x in fibonacci_generator(235):
     print(x)
+
+# oneliner
+'''from functools import reduce
+fib = lambda m: (a:=0, b:=1, [a := (tmp:=a, a:=b, b:=tmp+b)[0] for _ in range(m) if a <= m])[-1]
+
+
+(a:=0, b:=1, ...): Wir initialisieren a und b direkt im Tuple.
+
+tmp:=a, a:=b, b:=tmp+b: Das ist der Walrus-Trick, um das Tuple-Unpacking in einer List Comprehension zu simulieren.
+
+if a <= m: Hier wird das Ende bei 174 (oder deinem Wert m) erzwungen.
+
+[-1]: Am Ende nehmen wir das letzte Element des äußeren Tuples (unsere generierte Liste).
+
+print(fib(174))
+'''
+
+
+''' DAY 21 '''
+# reverse a list
+
+def reverse_a_list(list_objk):
+    rev_list = list(list_objk)
+    rev_counter = 4
+    for x in list_objk:
+        print(x)
+        rev_list[rev_counter] = x
+        rev_counter-=1
+    return rev_list
+
+def reverse_a_list_wrong(list_objk):
+    rev_list = list_objk # bildet eine reverence auf die liste keine Kopie
+    rev_counter = len(list_objk)-1
+    for x in list_objk:
+        rev_list[rev_counter] = x
+    return rev_list
+
+print(reverse_a_list(testlist))
+
+#pytonic way
+
+def reverse_the_list(list_objk):
+    return list_objk[::-1]
+
+print(reverse_the_list(testlist))
