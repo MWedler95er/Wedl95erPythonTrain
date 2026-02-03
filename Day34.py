53bdb6f430e3b84428afc16c26b0a2ae286aa7fb
MWedl95er <michael-wedler95@gmx.de>
2026-02-03 13:13:46 +0100

    textfile_r_w_a

---
+++ Day34.py
@@ -0,0 +1,16 @@
+'''
+
+            DAY 34
+        #Append data to an existing text file.
+
+'''
+
+def append_to_text_file(text_file_name,text_to_append):
+    with open(text_file_name,"a",encoding="utf-8") as txt_file:
+        txt_file.write(text_to_append)
+    return "DONE"
+
+file_name = "testText.txt"
+text_input = "A new line, A new line!\n"
+
+append_to_text_file(file_name,text_input)
