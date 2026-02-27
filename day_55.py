3345480989e17b0c054b26a93ce9b6b0054461a3
MWedl95er <michael-wedler95@gmx.de>
2026-02-27 14:02:33 +0100

    finish commit

--- day_55.py
+++ day_55.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Kartendeck:
     """Repräsentiert ein Kartendeck und steuert ein einfaches Blackjack-Spiel.
     Verwaltet Karten, Spieler- und Bankhände sowie den Ablauf einer Spielrunde.
@@ -8,10 +9,13 @@ class Kartendeck:
         None
 
     """
+
     def __init__(self):
         self.karten_werte = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Bube", "Dame", "König", "Ass"]
         self.karten_farben = ["❤️", "♠️", "♦️", "♣️"]
-        self.karten_deck = [[w, f] for f in self.karten_farben for w in self.karten_werte]
+        self.karten_deck = [
+            [w, f] for f in self.karten_farben for w in self.karten_werte
+        ]
         self.index = 0
         self.spieler_hand = []
         self.bank_hand = []
@@ -21,8 +25,7 @@ class Kartendeck:
             karte = self.karten_deck[self.index]
             self.index += 1
             return karte
-        else:
-            raise StopIteration
+        raise StopIteration
 
     def __iter__(self):
         self.index = 0
@@ -38,7 +41,7 @@ class Kartendeck:
     def wert_berechnen(self, hand):
         wert = 0
         asse = 0
-        for karte, farbe in hand:
+        for karte, farbe in hand:  # pylint: disable=unused-variable
             if karte in ["Bube", "Dame", "König"]:
                 wert += 10
             elif karte == "Ass":
@@ -46,7 +49,7 @@ class Kartendeck:
                 wert += 11
             else:
                 wert += karte
-        
+
         while wert > 21 and asse > 0:
             wert -= 10
             asse -= 1
@@ -54,18 +57,18 @@ class Kartendeck:
 
     def bank_spielt(self, spieler_punkte):
         self.bank_hand = [self.austeilen(), self.austeilen()]
-        
+
         while True:
             bank_punkte = self.wert_berechnen(self.bank_hand)
             hand_str = ", ".join([f"{k}{f}" for k, f in self.bank_hand])
             print(f"Bank Hand: {hand_str} (Punkte: {bank_punkte})")
 
-            if bank_punkte < 17 or spieler_punkte>bank_punkte:
+            if bank_punkte < 17 or spieler_punkte > bank_punkte:
                 print("Bank zieht noch eine Karte...")
                 self.bank_hand.append(self.austeilen())
             else:
                 break
-        
+
         # --- Finaler Vergleich ---
         if bank_punkte > 21:
             print("Bank hat sich überkauft! DU GEWINNST!")
@@ -79,31 +82,32 @@ class Kartendeck:
     def game_logik(self):
         self.karten_mischen()
         self.spieler_hand = [self.austeilen(), self.austeilen()]
-        
+
         while True:
             punkte = self.wert_berechnen(self.spieler_hand)
             hand_str = ", ".join([f"{k}{f}" for k, f in self.spieler_hand])
             print(f"\nDeine Hand: {hand_str} (Punkte: {punkte})")
-            
+
             if punkte == 21:
                 print("Blackjack! Du hast gewonnen!")
-                return # Spiel beendet
+                return  # Spiel beendet
             if punkte > 21:
                 print("Über 21! Du hast leider verloren.")
-                return # Spiel beendet
+                return  # Spiel beendet
 
             wahl = input("Noch eine [K]arte ziehen oder [H]alten? ").lower()
 
-            if wahl == 'k':
+            if wahl == "k":
                 neue_karte = self.austeilen()
                 self.spieler_hand.append(neue_karte)
                 print(f"Gezogen: {neue_karte[0]}{neue_karte[1]}")
-            elif wahl == 'h':
+            elif wahl == "h":
                 print(f"Du hältst bei {punkte} Punkten.")
                 self.bank_spielt(punkte)
                 break
             else:
                 print("Ungültige Eingabe!")
+
 
 spiel_deck = Kartendeck()
 
